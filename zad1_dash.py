13c1afe2efb65cd6f5f11f3671aca0f421daf7cb
wkulczi <killer98001@gmail.com>
2021-03-15 20:05:21 +0100

    still work in progress - random values are put into the table and graph

--- zad1_dash.py
+++ zad1_dash.py
@@ -3,24 +3,29 @@ import datetime
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly.graph_objects as go
 import dash_table
 import pandas as pd
 import plotly.express as px
 import plotly
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 import dash_bootstrap_components as dbc
 from dash.exceptions import PreventUpdate
 
-JupyterDash.infer_jupyter_proxy_config()
+import random
+from random_word import RandomWords
 
-data = pd.read_csv("untitled.csv")
+random_words = RandomWords()
+
+JupyterDash.infer_jupyter_proxy_config()
 
 app = JupyterDash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 app.layout = html.Div(
     [
         dcc.Interval(
-            id='interval-component',
+            id='tick-component',
+            n_intervals=0,
             disabled=True),
         dbc.Row(
             [
@@ -70,17 +75,33 @@ app.layout = html.Div(
                                     className='mx-1', width={"size": 4, "offset": 1}),
                             dbc.Col([dbc.Button("CLEAR", size='sm', block=True)], className='mx-1', width=4)
                         ], no_gutters=True, className='my-1'),
-                    ])
+                    ]),
+                    html.Div('', id='placeholder-div')
                     ###############################
                     # tutaj jakiś card z markdownem z opisem zadania czy coś
                     ###############################
                 ], width=6),
                 dbc.Col([
-                    dcc.Graph(),
-                    dash_table.DataTable(
-                        id='table',
-                        columns=[{"name": i, "id": i} for i in data.columns],
-                        data=data.to_dict('records'))
+                    dcc.Graph(id='graph-window', figure={}),
+                    dbc.Row([
+                        dbc.Col([
+                            dash_table.DataTable(
+                                id='table',
+                                columns=[{'id': 'step', 'name': 'step'}, {'id': 'value', 'name': 'value'}],
+                                data=[],
+                                page_size=10,
+                                style_table={'height': '350px', 'overflowY': 'auto'},
+                                style_cell_conditional=[
+                                    {
+                                        'if': {
+                                            'column_id': 'step',
+                                        },
+                                        'width': '10%'
+                                    }
+                                ],
+                                editable=True)
+                        ], width={"size": 5, "offset": 3})
+                    ])
                 ], width=6),
             ])
     ])
@@ -88,6 +109,8 @@ app.layout = html.Div(
 
 @app.callback(Output('stop-button', 'disabled'),
               Output('start-button', 'disabled'),
+              Output('tick-component', 'interval'),
+              Output('tick-component', 'disabled'),
               Input('start-button', 'n_clicks'),
               Input('stop-button', 'n_clicks'))
 def start_loop(start_nclicks, stop_nclicks):
@@ -96,12 +119,44 @@ def start_loop(start_nclicks, stop_nclicks):
         button_id = ctx.triggered[0]['prop_id'].split('.')[0]
         if button_id == 'start-button':
             #             ustaw period do timera i go odpal
-            #             potem callback timera musi zmieniać ten numerek
+            #             po'tem callback timera musi zmieniać ten numerek
             #             inny callback łapie ten numerek, robi krok, wstawia dane do tabeli i do grafu
-            return False, True
+            return False, True, 0.25 * 1000, False
         else:
             #            wyłącz timer
-            return True, False
+            return True, False, 0, True
+
+
+@app.callback(
+    Output('table', 'data'),
+    Input('tick-component', 'n_intervals'),
+    State('table', 'data'))
+def interval_tick(n_intervals, rows):
+    if n_intervals > 0:
+        rows.append({'step': n_intervals, 'value': random.randint(0, 22)})
+    return rows
+
+
+@app.callback(Output('table', 'page_current'),
+              Input('table', 'data'),
+              Input('table', 'page_size'))
+def auto_scroll(data, page_size):
+    return int((len(data) - 1) / page_size)
+
+
+@app.callback(Output('graph-window', 'figure'),
+              Input('table', 'data'),
+              State('table', 'data'))
+def update_graph(tableData, allData):
+    if allData:
+        fig = go.Figure(go.Scatter(
+            x=[i['step'] for i in allData],
+            y=[i['value'] for i in allData]
+        ))
+
+        return fig
+    else:
+        raise PreventUpdate
 
 
 app.run_server(debug=True)
