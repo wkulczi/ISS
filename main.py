defeaff1178c1b2e866890b3d0a1dafade7cc2d5
wkulczi <killer98001@gmail.com>
2021-03-29 20:34:17 +0200

    dodana klasa UAR, na razie tylko krok 4: wyznaczanie poziomu substancji

--- main.py
+++ main.py
@@ -2,19 +2,12 @@ from bokeh.document import Document
 from bokeh.io import show, curdoc
 from bokeh.models import TextInput, Button, Spinner, DataTable, ColumnDataSource, TableColumn
 from bokeh.plotting import figure
-import math
 
-# base values
-Tp = 1.0
-Qd = 1.0
-Beta = 1.0
-A = 1.0
-T = 1.0
-N = 0
-STEPS = 0
+from issmath.UAR import UAR
+
+uar = UAR()
 
 data = {"y": [], "x": []}
-qd_queue = []
 source = ColumnDataSource(data)
 graph_callback_task = None
 
@@ -25,35 +18,35 @@ clear_button = Button(label="Clear", button_type="success", name="clear_button",
 step_button = Button(label="Step", button_type="success", name="step_button", sizing_mode="stretch_width")
 send_button = Button(label="Send", button_type="success", name="send_button", sizing_mode="stretch_width")
 
-a_spinner = Spinner(low=0, value=1.0, step=0.2,    width_policy="min", name="a_spinner")
-h0_spinner = Spinner(low=0, value=1.0, step=0.2,   width_policy="min", high=20, name="h0_spinner")
+a_spinner = Spinner(low=0, value=1.0, step=0.2, width_policy="min", name="a_spinner")
+h0_spinner = Spinner(low=0, value=1.0, step=0.2, width_policy="min", high=20, name="h0_spinner")
 beta_spinner = Spinner(low=0, value=1.0, step=0.2, width_policy="min", name="beta_spinner")
-qd_spinner = Spinner(low=0, value=1.0, step=0.2,   width_policy="min", name="qd_spinner")
-tp_spinner = Spinner(low=0, value=0.5, step=0.2,   width_policy="min", name="tp_spinner")
-t_spinner = Spinner(low=0, value=30, step=0.2,     width_policy="min", name="t_spinner")
+qd_spinner = Spinner(low=0, value=1.0, step=0.2, width_policy="min", name="qd_spinner")
+tp_spinner = Spinner(low=0, value=0.5, step=0.2, width_policy="min", name="tp_spinner")
+t_spinner = Spinner(low=0, value=30, step=0.2, width_policy="min", name="t_spinner")
 
 p = figure(
     plot_height=120,
     name="bokeh_jinja_figure", sizing_mode="scale_width", toolbar_location="below")
 p.line(source=source, x='x', y='y', line_width=2, alpha=0.7)
-table = DataTable(sizing_mode="stretch_width", height_policy="max", name='table', source=source, columns=[TableColumn(field="y", title="H", sortable = False)],editable=True)
+table = DataTable(sizing_mode="stretch_width", height_policy="max", name='table', source=source,
+                  columns=[TableColumn(field="y", title="H", sortable=False)], editable=True)
 
 
 def start_button_handler():
     # load all values
-    load_global_values()
-
+    init_uar()
     start_callbacks()
 
 
-def load_global_values():
-    global Tp, Qd, Beta, A, T, N
-    Tp = tp_spinner.value
-    Qd = qd_spinner.value
-    Beta = beta_spinner.value
-    A = a_spinner.value
-    T = t_spinner.value
-    N = T / Tp
+def init_uar(reset_h = True):
+    uar.set_values(h0=h0_spinner.value,
+                   A=a_spinner.value,
+                   Tp=tp_spinner.value,
+                   t=t_spinner.value,
+                   Qdn=qd_spinner.value,
+                   beta=beta_spinner.value,
+                   reset_h = reset_h)
 
 
 def stop_button_handler():
@@ -69,44 +62,33 @@ def clear_button_handler():
 
 
 def step_button_handler():
-    load_global_values()
-    calculate_and_add_new_data()
-
-
-def calculate_next_h(previous_h):
-    global Tp, Qd, Beta, A, T, qd_queue
-    if qd_queue:
-        Qd = qd_queue.pop()
-    hn = (((-Beta * math.sqrt(previous_h) + Qd) * Tp) / A) + previous_h
-    return float("{:.2f}".format(hn))
+    init_uar(reset_h = False)
+    calc_step()
 
 
 def calculate_new_data_if_N():
-    global STEPS, N
-    if STEPS < int(N):
-        calculate_and_add_new_data()
-        STEPS += 1
+    if not uar.should_stop():
+        calc_step()
     else:
         stop_callbacks()
 
 
-def calculate_and_add_new_data():
-    h0 = h0_spinner.value
+def calc_step():
+    new_h = uar.count_and_add_h_step(return_value=True)
+    append_to_table(float("{:.2f}".format(new_h)))
 
+
+def append_to_table(new_h):
     new_data = {'x': [], 'y': []}
-    if len(data['x']) < 1:
-        h = h0
-    else:
-        h = calculate_next_h(data['y'][-1])
     new_data['x'].append(len(data['x']))
-    new_data['y'].append(h)
+    new_data['y'].append(new_h)
 
     source.stream(new_data=new_data)
 
 
 def start_callbacks():
     global graph_callback_task
-    if STEPS < int(N):
+    if not uar.should_stop():
         graph_callback_task = curdoc().add_periodic_callback(calculate_new_data_if_N, 1000)
 
         step_button.disabled = True
@@ -122,8 +104,7 @@ def start_callbacks():
 
 
 def stop_callbacks():
-    global STEPS, graph_callback_task
-    STEPS = 0
+    uar.reset_steps()
     curdoc().remove_periodic_callback(graph_callback_task)
 
     start_button.disabled = False
@@ -139,16 +120,9 @@ def stop_callbacks():
 
 
 def apply_qd_change():
-    global Qd
-    change_value = qd_spinner.value - Qd
-    if abs(change_value) > 0.5:
-        change_step = change_value / 2
-        print("applying change. New value should be: {}, change: {}, half the change is: {}".format(qd_spinner.value,
-                                                                                                    change_value,
-                                                                                                    change_step))
-        qd_queue.extend([qd_spinner.value, Qd + change_step])
-    else:
-        Qd = qd_spinner.value
+    print(qd_spinner)
+    print(type(qd_spinner))
+    uar.set_Qdn(qd_spinner.value)
 
 
 # component callbacks
